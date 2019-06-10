f827de5ae44cb5401c8a328b130428d170bcd08c
Steven Friend <stevenmarkfriend@gmail.com>
2019-06-10 17:20:19 +0100

    Implemented eye detection using Haar Cascades

--- smart_video_player.py
+++ smart_video_player.py
@@ -1,5 +1,6 @@
 import cv2
 
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
@@ -11,10 +12,17 @@ while True:
         break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
     for(x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 6)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cropped_face = frame[y : y + h, x : x + w]
+        cropped_face_gray = gray[y : y + h, x : x + w]
+        eyes = eye_cascade.detectMultiScale(cropped_face_gray, 1.1, 5)
+        for (ex, ey, ew, eh) in eyes:
+            curr_sate = True
+            cv2.rectangle(cropped_face, (ex, ey),
+                (ex + ew, ey + eh), (0, 255, 0), 2)
 
     cv2.imshow('Face detection', frame)
 
